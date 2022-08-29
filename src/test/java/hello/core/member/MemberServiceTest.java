07a1826a6d0bb816a571b8a39b16e0a5752c0695
portuna85 <portuna85@gmail.com>
2022-08-29 15:48:07 +0900

    Initial commit

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,33 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+
+    @BeforeEach
+    void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+    @Test
+    public void 회원가입() throws Exception {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        // then
+        assertThat(member).isEqualTo(findMember);
+
+    }
+}
